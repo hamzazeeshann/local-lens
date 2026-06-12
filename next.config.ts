d877eb5e797a34a89f7d41aee9697dc4cf600353
Hamza Zeeshan Ahmed <bsai24022@itu.edu.pk>
2026-06-12 20:42:47 +0500

    deployment fixes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["leaflet", "react-leaflet"],
-  serverExternalPackages: ["bcryptjs"],
+  serverExternalPackages: ["bcryptjs", "pg"],
 };
 
 export default nextConfig;
